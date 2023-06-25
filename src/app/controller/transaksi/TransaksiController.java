1531f04bde72215ce5e4fa021d14fa9a23616a8f
Ade Hikmat Fauzi Ridwan <aalolxyz@gmail.com>
2023-06-25 14:56:03 +0700

    init layer controller

--- src/app/controller/transaksi/TransaksiController.java
+++ src/app/controller/transaksi/TransaksiController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package app.controller;
+package app.controller.transaksi;
 
 /**
  *
  * @author Administrator
  */
-public class DemoController {
+public class TransaksiController {
     
 }
